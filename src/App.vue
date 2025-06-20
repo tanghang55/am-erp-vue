a11b26645bd3718eed28bfcc91ff81623a63478a
tangsky <474352380@qq.com>
2025-06-20 21:16:17 +0800

    Initialize Vue 3 project with Vite

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+    <h1>Hello Element Plus</h1>
+    <el-button type="primary">Primary Button</el-button>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+</style>
