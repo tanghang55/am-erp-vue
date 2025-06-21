b7a2dd1ee6eb5169a7fe65cadcc8dafe3bf491c1
tangsky <474352380@qq.com>
2025-06-21 13:33:07 +0800

    Add ESLint and Prettier configuration

---
+++ eslint.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'prettier'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  rules: {
+  },
+}
