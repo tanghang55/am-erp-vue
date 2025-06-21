776228c863030dc3a5d77ccc4ac637a9f72b551a
tangsky <474352380@qq.com>
2025-06-21 14:21:49 +0800

    Add login and register pages

---
+++ src/modules/Login.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="login-page">
+    <el-form @submit.prevent="onSubmit" :model="form" class="login-form">
+      <el-form-item label="Username">
+        <el-input v-model="form.username" autocomplete="off" />
+      </el-form-item>
+      <el-form-item label="Password">
+        <el-input v-model="form.password" type="password" autocomplete="off" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" native-type="submit">Login</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script setup>
+import { reactive } from 'vue'
+
+const form = reactive({
+  username: '',
+  password: ''
+})
+
+const onSubmit = () => {
+  console.log('login', form)
+  // TODO: implement login logic
+}
+</script>
+
+<style scoped>
+.login-page {
+  max-width: 400px;
+  margin: 2rem auto;
+}
+</style>
